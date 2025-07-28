2e0f8063dc4ddc388015a1faaca6f8d0fa3ee783
shchinova <shchinova18@gmail.com>
2025-07-28 16:33:26 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-<script>
-  window.addEventListener("scroll", function () {
-    const header = document.getElementById("main-header");
-    if (window.scrollY > 50) {
-      header.classList.add("shrink");
-    } else {
-      header.classList.remove("shrink");
-    }
-  });
-</script>
+// Пока что скрипт пустой.
+// Можешь добавить, например, плавную прокрутку или интерактивность проектов.
+console.log("Сайт загружен.");
